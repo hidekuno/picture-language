6095440ea44034c020d093b5e08af95cfd65f4ec
hidekuno <hidekuno@gmail.com>
2019-11-01 16:41:25 +0900

    demoプログラム修正

--- fractal/dragon.scm
+++ fractal/dragon.scm
@@ -3,9 +3,11 @@
 ;
 ; hidekuno@gmail.com
 ;
-(define (dragon-curve-demo n frame)
+(define (dragon-curve-demo n)((dragon-curve n) frame))
+
+(define (dragon-curve-geometric-demo n)
   ((transform-painter
     (dragon-curve n)
     (make-vect 0.0 0.0)
-    (make-vect 1.0 0.0)
-    (make-vect 0.0 1.0)) frame))
+    (make-vect 0.75 0.25)
+    (make-vect 0.25 0.75)) frame))
--- fractal/koch.scm
+++ fractal/koch.scm
@@ -3,9 +3,11 @@
 ;
 ; hidekuno@gmail.com
 ;
-(define (koch-demo n frame)
+(define (koch-demo n)((koch n) frame))
+
+(define (koch-geometric-demo n)
   ((transform-painter
     (koch n)
     (make-vect 0.0 0.0)
-    (make-vect 1.0 0.0)
-    (make-vect 0.0 1.0)) frame))
+    (make-vect 0.75 0.25)
+    (make-vect 0.25 0.75)) frame))
